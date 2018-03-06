a60434e1b521be345afb6cc72860139de955222e
danielcev <daniel_plata_apps1m1617@cev.com>
2018-03-06 14:46:07 +0100

    Cambio en buscador

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 const URLPRO = "http://h2744356.stratoserver.net/solfamidas/alumniCEV/public/index.php/";
 const URLDEV = "http://localhost:8888/alumniCEV/public/";
 
-const URL = URLDEV;
+const URL = URLPRO;
